8ad1bea111e119bb2e92f7b0fba487f9a6b8b1d2
joegruss <106345744+joegruss@users.noreply.github.com>
2024-12-11 11:19:41 -0500

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,8 +1,8 @@
 @echo off
 echo Copy the 'terastitcher' directory to the C: drive and add 'C:\terastitcher' to the PATH environment variable, then press any key to continue.
 pause
-echo Download and run the BioFormats installer at: https://viewer.imaris.com/download/ImarisConvertBioformats_Latest.exe, then press any key to continue.
-pause
+@REM echo Download and run the BioFormats installer at: https://viewer.imaris.com/download/ImarisConvertBioformats_Latest.exe, then press any key to continue.
+@REM pause
 
 @REM del /s /f /q c:\terastitcher\*.*
 @REM for /f %%f in ('dir /ad /b c:\terastitcher\') do rd /s /q c:\terastitcher\%%f
