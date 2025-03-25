2cc37b8c23d793c83d9c40165d5200c2a5bdf8b3
Minyoung E. Kim <mykim@alumni.stanford.edu>
2025-03-25 15:39:50 -0400

    Update update_pp_log.py
    add full working_directory path

--- Stitch GUI/StitchGUI_Config/update_pp_log.py
+++ Stitch GUI/StitchGUI_Config/update_pp_log.py
@@ -3,14 +3,15 @@ import os
 import sys
 
 # Get arguments from batch script
-if len(sys.argv) < 5:
-    print("Usage: python update_json.py <channel> <current_dir> <file_count> <json_file>")
+if len(sys.argv) < 6:
+    print("Usage: python update_json.py <work_dir> <channel> <current_dir> <file_count> <json_file>")
     sys.exit(1)
 
-channel = sys.argv[1]  # e.g., "Ex_639_Ch3"
-current_dir = sys.argv[2] # e.g., "Ex_639_Ch3_stitched"
-file_count = int(sys.argv[3])  # e.g., 3000
-json_file = sys.argv[4]  # Path to JSON file (e.g., "C:/path/to/output.json")
+work_dir = sys.argv[1]
+channel = sys.argv[2]  # e.g., "Ex_639_Ch3"
+current_dir = sys.argv[3] # e.g., "Ex_639_Ch3_stitched"
+file_count = int(sys.argv[4])  # e.g., 3000
+json_file = sys.argv[5]  # Path to JSON file (e.g., "C:/path/to/output.json")
 
 # Ensure JSON file exists or create an empty dictionary
 if os.path.exists(json_file):
@@ -24,6 +25,7 @@ else:
 
 # Update or add the new entry
 data[channel] = {
+    "work_directory": work_dir,
     "current_directory": current_dir,
     "file_count": file_count
 }
