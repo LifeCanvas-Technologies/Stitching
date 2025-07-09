39e0712d8029f2d1beba8df0c4cccdef339317b9
joegruss <106345744+joegruss@users.noreply.github.com>
2025-07-09 16:18:21 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -6,22 +6,6 @@ pause
 echo Download and run the BioFormats installer at: https://github.com/imaris/ImarisConvertBioformats, then press any key to continue.
 pause
 
-@REM del /s /f /q c:\terastitcher\*.*
-@REM for /f %%f in ('dir /ad /b c:\terastitcher\') do rd /s /q c:\terastitcher\%%f
-@REM echo f | xcopy /s /e /q ".\terastitcher" "C:\terastitcher"
-@REM pause
-
-@REM echo Copy the 'Stitch GUI\SmartSPIM' folder to 'C:\Program Files\SmartSPIM'
-@REM pause
-@REM echo Create an empty 'c:\Program Files\SmartSPIM' folder if it doesn't exist.
-@REM pause
-
-@REM del /s /f /q C:\Program Files\SmartSPIM*.*
-@REM for /f %%f in ('dir /ad /b C:\Program Files\SmartSPIM\') do rd /s /q C:\Program Files\SmartSPIM\%%f
-@REM echo f | xcopy /s /e /q ".\Stitch GUI\SmartSPIM" "C:\Program Files\SmartSPIM"
-@REM pause
-
-
 start /wait msmpisetup.exe
 start /wait VC_redist.x64.exe
 start /wait vcredist_x64.exe
@@ -29,8 +13,10 @@ start /wait .\LVRTE2017SP1_f4Patch-64std\setup.exe
 start /wait ni-vision-runtime_20.7_online.exe
 
 call conda env remove -n stitching
+call conda env remove -p C:\ProgramData\Anaconda3\envs\stitching
 
 call conda create -p C:\ProgramData\Anaconda3\envs\stitching python=3.8
+icacls C:\ProgramData\Anaconda3\envs\stitching /grant Everyone:(RX) /T
 
 call conda activate stitching
 
